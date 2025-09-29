651e0c40f14eddc386daad64fa0e8eed725234cb
zmtmqhdl <zmtmqhdl@naver.com>
2025-09-29 22:20:59 +0900

    Railway

--- src/main/kotlin/database/DatabaseFactory.kt
+++ src/main/kotlin/database/DatabaseFactory.kt
@@ -6,27 +6,27 @@ import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 
-fun connectToDatabase() {
-    val config = HikariConfig().apply {
-        jdbcUrl = "jdbc:postgresql://localhost:5432/BlindTrueOrDare"
-        driverClassName = "org.postgresql.Driver"
-        username = "postgres"
-        password = "xogns1469"
-        maximumPoolSize = 10
-        isAutoCommit = false
-        transactionIsolation = "TRANSACTION_REPEATABLE_READ"
-        validate()
-    }
-
-    val dataSource = HikariDataSource(config)
-    Database.connect(dataSource)
-
-    transaction {
-        SchemaUtils.createMissingTablesAndColumns(
-            Player,
-            WaitingRoom,
-            ParticipantLists,
-            Question
-        )
-    }
-}
+//fun connectToDatabase() {
+//    val config = HikariConfig().apply {
+//        jdbcUrl = "jdbc:postgresql://localhost:5432/BlindTrueOrDare"
+//        driverClassName = "org.postgresql.Driver"
+//        username = "postgres"
+//        password = "xogns1469"
+//        maximumPoolSize = 10
+//        isAutoCommit = false
+//        transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+//        validate()
+//    }
+//
+//    val dataSource = HikariDataSource(config)
+//    Database.connect(dataSource)
+//
+//    transaction {
+//        SchemaUtils.createMissingTablesAndColumns(
+//            Player,
+//            WaitingRoom,
+//            ParticipantLists,
+//            Question
+//        )
+//    }
+//}
--- src/main/kotlin/database/Table.kt
+++ src/main/kotlin/database/Table.kt
@@ -4,32 +4,32 @@ import org.example.model.RoomStatus
 import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.Table
 
-object Player : Table("player") {
-    val playerId = uuid("player_id")
-    val nickname = text("nickname")
-
-    override val primaryKey = PrimaryKey(playerId)
-}
-
-object WaitingRoom : Table("waiting_room") {
-    val roomId = uuid("room_id")
-    val roomStatus = enumerationByName("waiting_room_status", 10, RoomStatus::class)
-}
-
-object ParticipantLists : Table("participant_list") {
-    val roomId = uuid("room_id").references(WaitingRoom.roomId, onDelete = ReferenceOption.CASCADE)
-    val playerId = uuid("player_id").references(Player.playerId, onDelete = ReferenceOption.CASCADE)
-
-    override val primaryKey = PrimaryKey(roomId, playerId)
-}
-
-object Question : Table("question") {
-    val questionId = uuid("question_id")
-    val roomId = uuid("room_id").references(WaitingRoom.roomId, onDelete = ReferenceOption.CASCADE)
-    val playerId = uuid("player_id").references(Player.playerId, onDelete = ReferenceOption.SET_NULL).nullable()
-    val text = text("text")
-    val oCount = integer("o_count").default(0)
-    val xCount = integer("x_count").default(0)
-
-    override val primaryKey = PrimaryKey(questionId)
-}
+//object Player : Table("player") {
+//    val playerId = uuid("player_id")
+//    val nickname = text("nickname")
+//
+//    override val primaryKey = PrimaryKey(playerId)
+//}
+//
+//object WaitingRoom : Table("waiting_room") {
+//    val roomId = uuid("room_id")
+//    val roomStatus = enumerationByName("waiting_room_status", 10, RoomStatus::class)
+//}
+//
+//object ParticipantLists : Table("participant_list") {
+//    val roomId = uuid("room_id").references(WaitingRoom.roomId, onDelete = ReferenceOption.CASCADE)
+//    val playerId = uuid("player_id").references(Player.playerId, onDelete = ReferenceOption.CASCADE)
+//
+//    override val primaryKey = PrimaryKey(roomId, playerId)
+//}
+//
+//object Question : Table("question") {
+//    val questionId = uuid("question_id")
+//    val roomId = uuid("room_id").references(WaitingRoom.roomId, onDelete = ReferenceOption.CASCADE)
+//    val playerId = uuid("player_id").references(Player.playerId, onDelete = ReferenceOption.SET_NULL).nullable()
+//    val text = text("text")
+//    val oCount = integer("o_count").default(0)
+//    val xCount = integer("x_count").default(0)
+//
+//    override val primaryKey = PrimaryKey(questionId)
+//}
